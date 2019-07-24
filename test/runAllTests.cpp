dcb921085c52db10367cb739e606c67814064d59
Guido Brini <34924256+devguy1701@users.noreply.github.com>
2019-07-24 13:30:17 +0200

    Aggiunta riconoscimento libreria googletest

---
+++ test/runAllTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by guido on 24/07/2019.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
